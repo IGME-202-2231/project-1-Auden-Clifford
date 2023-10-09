9ec8d772f7e7384f1fd169e9a253718add5fac6f
Auden-Clifford <112361970+Auden-Clifford@users.noreply.github.com>
2023-10-08 22:26:39 -0400

    started InputController script

--- Project 1/Assets/Scripts/InputController.cs
+++ Project 1/Assets/Scripts/InputController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.InputSystem;
 
 public class InputController : MonoBehaviour
 {
@@ -15,4 +16,16 @@ public class InputController : MonoBehaviour
     {
         
     }
+    /*
+    // The method that gets called to handle any player movement input
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        // Get the latest value for the input from the Input System
+        inputDirection = context.ReadValue<Vector2>();  // This is already normalized for us
+
+        // Send that new direction to the Vehicle class
+        myMovmentController.SetDirection(inputDirection);
+    }
+    */
+
 }
