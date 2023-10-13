4744c9272891d4f88314e556774b653a444cc8e3
Auden-Clifford <112361970+Auden-Clifford@users.noreply.github.com>
2023-10-12 22:43:06 -0400

    created working infinite background

--- Project 1/Assets/Scripts/BackgroundController.cs
+++ Project 1/Assets/Scripts/BackgroundController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class BackgroundController : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+
+    //[SerializeField] private List<GameObject> backgroundPanels;
     //[SerializeField] private GameObject backgroundPanel;
 
     private SpriteRenderer backgroundImage;
-    
+
+    // this (should) be the accurate height and width of the background panels
+    private Vector2 backgroundSize = new Vector2(24.8f, 15.5f);
 
     private void Start()
     {
@@ -19,6 +23,7 @@ public class BackgroundController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         // chack if the player has crossed out of the bounds of the center background pannel
         if(player.transform.position.x >= this.transform.position.x + (backgroundImage.size.x / 2) || // has the player gone beyond the right edge? 
             player.transform.position.x <= this.transform.position.x - (backgroundImage.size.x / 2) || // has the player gone beyond the left edge?
@@ -27,5 +32,51 @@ public class BackgroundController : MonoBehaviour
         {
 
         }
+        */
+
+        // check if the player is beyond gone beyond the right edge
+        if (player.transform.position.x >= this.transform.position.x + (backgroundSize.x / 2)) 
+        {
+            // move the background right by 1 background panel width
+            this.transform.position = new Vector2(
+                this.transform.position.x + backgroundSize.x,
+                this.transform.position.y); // background y value is preserved
+
+            print(this.transform.position.x);
+        }
+
+        // check if the player is beyond gone beyond the left edge
+        if (player.transform.position.x <= this.transform.position.x - (backgroundSize.x / 2)) 
+        {
+            // move the background left by 1 background panel width
+            this.transform.position = new Vector2(
+                this.transform.position.x - backgroundSize.x,
+                this.transform.position.y); // background y value is preserved
+
+            print(this.transform.position.x);
+        }
+
+        // check if the player is beyond gone above the top edge
+        if (player.transform.position.y >= this.transform.position.y + (backgroundSize.y / 2))
+        {
+            // move the background up by 1 background panel width
+            this.transform.position = new Vector2(
+                this.transform.position.x, // background x is preserved
+                this.transform.position.y + backgroundSize.y);
+        }
+
+        // check if the player is beyond gone below the bottom edge
+        if (player.transform.position.y <= this.transform.position.y - (backgroundSize.y / 2))
+        {
+            // move the background down by 1 background panel width
+            this.transform.position = new Vector2(
+                this.transform.position.x, // background x is preserved
+                this.transform.position.y - backgroundSize.y);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawWireCube(this.transform.position, backgroundSize);
     }
 }
